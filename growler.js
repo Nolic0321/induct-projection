f357b1930b29b8e7f186c5acf6e53af934832f69
Nolic0321 <nolic0321@gmail.com>
2019-09-19 05:04:46 -0500

    Moved rate debug to bottom of page

--- growler.js
+++ growler.js
@@ -1,63 +1,150 @@
 var growler = new Vue({
+
     el: "#growler",
+
     data: {
+
         totalPackages: 0,
+
         inductRates: [],
+
         totalInducted: 0,
+
         inductRate: 0,
+
         today: new Date(),
+
         estimatedTimeComplete: new Date().toLocaleTimeString(),
+
         projectedInducted: 0,
+
         projectedLeft: 0,
+
         actualLeft: 0,
+
         inductDifference: 0,
+
         inductionStarted: false
+,
+	startTime: null
     },
+
     methods: {
         beginInduction: function () {
-            this.addInductRate(0)
+
+            //this.addInductRate(0)
+
+	    this.projectedLeft = this.totalPackages
             this.inductionStarted = true
-        },
+
+	    this.startTime = new Date().getTime()
+	    this.queueNextRequest()
+       },
+
         calculateEstimatedTime: function () {
+
             this.updateActualLeft()
+
             var today = new Date();
+
             if (this.inductRates.length > 2) {
+
                 var minutesRemaining = this.minutesRemaining()
+
                 today.setMinutes(today.getMinutes() + minutesRemaining)
+
             }
+
             growler.estimatedTimeComplete = today.toLocaleTimeString();
+
         },
+
         addInductRate: function (inductRate) {
-            growler.inductRates.push({ rate: inductRate, time: new Date().getTime() })
+	    var currentTime = new Date().getTime()
+            growler.inductRates.push({ rate: inductRate, time: currentTime , elapsedTime: this.inductRates.length == 0 ? 0 : currentTime - this.inductRates[this.inductRates.length-1].time })
+
             this.calculateEstimatedTime()
+
+	    this.updateProjectedInduct()
         },
-        minutesRemaining: function () {
-            var aveRate = this.inductRates.reduce((total, n) => total + n.rate, 0)
+
+	updateProjectedInduct: function(){
+		this.projectedInducted = Math.round(this.getAverageRate() / 60 / 60 / 1000 * (new Date().getTime() - this.startTime))
+		this.calculateEstimatedTime()
+	},
+	getAverageRate: function(){
+	    var aveRate = this.inductRates.reduce((total, n) => total + n.rate, 0)
+
 
             aveRate = aveRate / this.inductRates.length
-            return Number.isNaN(aveRate) ? 0 : this.actualLeft / aveRate * 60
+
+	    return aveRate
+	},
+	queueNextRequest: function(){
+		console.log("Queueing Request")
+		setTimeout( () => {
+				var a = this
+				a.requestInductData()
+			},
+			3000)
+	},
+	requestInductData: function(){
+		//https://logistics.amazon.com/station/induct/data
+		//currResponseData.value
+		axios
+			.get("https://logistics.amazon.com/station/induct/data", {crossdomain:true})
+			.then(response => {
+						this.addInductRate(response.curreResponseData.value)
+						this.queueNextRequest()
+					  })
+	},
+        minutesRemaining: function () {
+
+            return Number.isNaN(this.getAverageRate())? 0 : this.actualLeft / this.getAverageRate() * 60
+
         },
+
         updateTotalPackages: function (newTotal) {
+
             this.totalPackages = newTotal
+
             this.updateActualLeft()
             //Update current values to reflect new total changes
+
         },
+
         updateActualLeft: function () {
+
             this.actualLeft = this.totalPackages - this.totalInducted
+
             this.inductDifference = this.projectedInducted - this.totalInducted
+
         },
         calculateProjectedInducted: function () {
+
             var aveRate = this.inductRates.reduce((total, n) => total + n.rate, 0)
+
             var timeElapsed = 0
+
         },
-        totalInductedChanged: function(event){
-            clearInterval(timeout)
+
+
+	getNiceTime: function(time){
+		var dateTime = new Date(time);
+		//return dateTime.getHours() + ":" + dateTime.getMinutes() + ":" + dateTime.getSeconds()
+		return dateTime.toLocaleTimeString()
+	},
+        updateTotalInducted: function(){
+
             if (event){
+
                 console.log(event);
+
             }
-            timeout = setTimeout(() => {this.calculateEstimatedTime()},1000)
+
+            setTimeout(() => {this.calculateEstimatedTime()},1000)
+
         }
     }
 })
 
-var timeout = null
