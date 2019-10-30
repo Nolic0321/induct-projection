7bdd7336f6627dbba57e786aeba0b7277356b6c9
Collin Gowdy <nolic0321@gmail.com>
2019-10-30 05:08:48 -0500

    Forgot to add files

--- task-boarder/app.js
+++ task-boarder/app.js
@@ -1,3 +1,15 @@
+const daysOfWeek = {
+    SUNDAY: 0,
+    MONDAY: 1,
+    TUESDAY: 2,
+    WEDNESDAY: 3,
+    THURSDAY: 4,
+    FRIDAY: 5,
+    SATURDAY: 6
+}
+
 var breakTracker = new Vue({
-    el: 'task-boarder'
+    el: 'task-boarder',
+    data:{},
+    methods:{}
 })
