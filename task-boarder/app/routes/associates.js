613219dcd723a63bf4786d3f99abd53188179a40
Collin Gowdy <nolic0321@gmail.com>
2019-11-07 05:36:01 -0600

    Formatting

--- task-boarder/app/routes/associates.js
+++ task-boarder/app/routes/associates.js
@@ -31,27 +31,18 @@ module.exports = function(app, db) {
 	// Create table on load
 	db.createTable(createAssocaiteTableParams, function(err, data) {
 		if (err) {
-			console.error(
-				'Unable to create table. Error JSON:',
-				JSON.stringify(err, null, 2)
-			);
+			console.error('Unable to create table. Error JSON:', err.message);
 		} else {
-			console.log(
-				'Created table. Tagble description JSON:',
-				JSON.stringify(data, null, 2)
-			);
+			console.log('Created table. Tagble description JSON:', JSON.stringify(data, null, 2));
 		}
 	});
 
-	// Add associate
+	// Create associate
 	app.post('/associates/:login', (req, res) => {
 		var login = req.params.login;
 		db.putItem(postPuAssociateParams(login), (err, data) => {
 			if (err) {
-				console.error(
-					'Unable to create ' + login + '. Error JSON:',
-					JSON.stringify(err, null, 2)
-				);
+				console.error('Unable to create ' + login + '. Error JSON:', JSON.stringify(err, null, 2));
 				res.send(err);
 			} else {
 				console.log('Added item:', JSON.stringify(data, null, 2));
@@ -60,14 +51,11 @@ module.exports = function(app, db) {
 		});
 	});
 
-	// Get all associates
+	// Request all associates
 	app.get('/associates', (req, res) => {
 		db.scan({ TableName: tableName }, (err, data) => {
 			if (err) {
-				console.error(
-					'Unable to find associates table Error JSON:',
-					JSON.stringify(err, null, 2)
-				);
+				console.error('Unable to find associates table Error JSON:', JSON.stringify(err, null, 2));
 				send(err);
 			} else {
 				console.log(JSON.stringify(data, null, 2));
@@ -76,21 +64,15 @@ module.exports = function(app, db) {
 		});
 	});
 
-	// Get associate with login :login
+	// Request associate with login :login
 	app.get('/associates/:login', (req, res) => {
 		var login = req.params.login;
 		db.getItem(getAssociateParams(login), (err, data) => {
 			if (err) {
-				console.error(
-					'Unable to find ' + login + '. Error JSON:',
-					JSON.stringify(err, null, 2)
-				);
+				console.error('Unable to find ' + login + '. Error JSON:', JSON.stringify(err, null, 2));
 				res.send(err);
 			} else {
-				console.log(
-					'GetItem succeeded:',
-					JSON.stringify(data, null, 2)
-				);
+				console.log('GetItem succeeded:', JSON.stringify(data, null, 2));
 				res.send(data);
 			}
 		});
@@ -110,7 +92,7 @@ module.exports = function(app, db) {
 		});
 	});
 
-	// Remove associate :login
+	// Delete associate :login
 	app.delete('/associates/:login', (req, res) => {
 		var login = req.params.login;
 		db.deleteItem(getAssociateParams(login), (err, data) => {
