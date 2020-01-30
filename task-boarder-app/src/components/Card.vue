8f80b8ad6445b9d67ffa718268c54e79bd540857
Collin Gowdy <nolic0321@gmail.com>
2020-01-30 05:08:05 -0600

    Started working on a Card component; package updates

---
+++ task-boarder-app/src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Name: {{associate.name}}
+    <br />
+    Login: {{associate.login}}
+    <br />
+    Workdays: {{associate.workdays}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card",
+  props: ["associate"]
+};
+</script>
