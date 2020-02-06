3338d77b1fef1293d9ac52a88b8f1ea32b00f31e
Collin Gowdy <nolic0321@gmail.com>
2020-02-06 07:02:24 -0600

    Added 'placeable' class Cards can be placed Known bug with card 'reset'

--- task-boarder-app/src/components/Card.vue
+++ task-boarder-app/src/components/Card.vue
@@ -1,32 +1,58 @@
 <style scoped>
 .card {
-  border: solid 1px black;
-  background-color: aliceblue;
-  display: inline-block;
-  flex-basis: content;
-  margin: 2px;
-  padding: 2px;
-  position: relative;
+	border: solid 1px black;
+	background-color: aliceblue;
+	display: inline-block;
+	flex-basis: content;
+	margin: 2px;
+	padding: 2px;
+	position: relative;
 }
 </style>
 
 <template>
-  <div class="card" v-draggable>
-    Name: {{ associate.name }}
-    <br />
-    Login: {{ associate.login }}
-    <br />
-    Workdays: {{ associate.workdays }}
-  </div>
+	<div class="card" v-draggable="draggableValue">
+		Name: {{ associate.name }}
+		<br />
+		Login: {{ associate.login }}
+		<br />
+		Workdays: {{ associate.workdays }}
+	</div>
 </template>
 
 <script>
-import { Draggable } from "draggable-vue-directive";
+import { Draggable } from 'draggable-vue-directive';
 export default {
-  name: "card",
-  props: ["associate"],
-  directives: {
-    Draggable
-  }
+	name: 'card',
+	props: ['associate'],
+	directives: {
+		Draggable
+	},
+	data() {
+		return {
+			draggableValue: {
+				handle: undefined,
+				onDragEnd: this.onDragEnd
+			}
+		};
+	},
+	methods: {
+		onDragEnd: function(positionDiff, absolutePosition, event) {
+			var validDrop = false;
+			var elements = document.elementsFromPoint(event.clientX, event.clientY);
+			for (var x = 0; x < elements.length; x++) {
+				var element = elements[x];
+				if (element.classList.contains('placeable')) {
+					element.appendChild(this.$el);
+					validDrop = true;
+					console.log('Dropped on ' + element.localName);
+				}
+			}
+			this.$el.style.position = 'inherit';
+			if (!validDrop) {
+				this.draggableValue.position = absolutePosition - positionDiff;
+			}
+		}
+	}
 };
 </script>
--- task-boarder-app/src/views/TaskBoard.vue
+++ task-boarder-app/src/views/TaskBoard.vue
@@ -1,225 +1,229 @@
 <style scoped>
 table {
-  border-collapse: collapse;
+	border-collapse: collapse;
 }
 td,
 th {
-  padding: 3px;
-  border: solid 1px;
-  width: 200px;
-  height: 50px;
+	padding: 3px;
+	border: solid 1px;
+	width: 200px;
+	height: 50px;
 }
 td {
-  vertical-align: top;
+	vertical-align: top;
 }
 
 .not-available {
-  background: black;
+	background: black;
 }
 
 .flex-container {
-  display: flex;
-  flex-wrap: wrap;
+	display: flex;
+	flex-wrap: wrap;
 }
 </style>
 
 <template>
-  <div>
-    <div>
-      <table title="Stow">
-        <caption>
-          <h1>Stow Path</h1>
-        </caption>
-        <tr>
-          <th>Stow</th>
-          <th>A</th>
-          <th>B</th>
-          <th>C</th>
-          <th>D</th>
-          <th>E</th>
-          <th>G</th>
-          <th>H</th>
-          <th>J</th>
-        </tr>
-        <tr>
-          <th>1-2</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>3-4</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>5-6</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>7-8</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>9-10</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>Divert</th>
-          <td></td>
-          <td class="not-available"></td>
-          <td></td>
-          <td></td>
-          <td>
-            <caption>floor</caption>
-          </td>
-          <td>
-            <caption>dock</caption>
-          </td>
-          <td class="not-available"></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>Stage</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>Stage</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </table>
+	<div>
+		<div>
+			<table title="Stow">
+				<caption>
+					<h1>Stow Path</h1>
+				</caption>
+				<tr>
+					<th>Stow</th>
+					<th>A</th>
+					<th>B</th>
+					<th>C</th>
+					<th>D</th>
+					<th>E</th>
+					<th>G</th>
+					<th>H</th>
+					<th>J</th>
+				</tr>
+				<tr>
+					<th>1-2</th>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+				</tr>
+				<tr>
+					<th>3-4</th>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+				</tr>
+				<tr>
+					<th>5-6</th>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+				</tr>
+				<tr>
+					<th>7-8</th>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+				</tr>
+				<tr>
+					<th>9-10</th>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+				</tr>
+				<tr>
+					<th>Divert</th>
+					<td class="placeable"></td>
+					<td class="not-available"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable">
+						<caption>
+							floor
+						</caption>
+					</td>
+					<td class="placeable">
+						<caption>
+							dock
+						</caption>
+					</td>
+					<td class="not-available"></td>
+					<td class="placeable"></td>
+				</tr>
+				<tr>
+					<th>Stage</th>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+				</tr>
+				<tr>
+					<th>Stage</th>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+				</tr>
+			</table>
 
-      <table>
-        <caption>
-          <h1>Dock</h1>
-        </caption>
-        <tr>
-          <th></th>
-          <th>Belt 1</th>
-          <th>Belt 2</th>
-          <th>Belt 3</th>
-          <th>Belt 4</th>
-          <th>Belt 5</th>
-        </tr>
-        <tr>
-          <th>Unload</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>Induct</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>Divert</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th>Water Spider</th>
-          <td colspan="5"></td>
-        </tr>
-      </table>
-    </div>
-    <div>
-      <h1>AA's</h1>
-      <div class="flex-container">
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
+			<table>
+				<caption>
+					<h1>Dock</h1>
+				</caption>
+				<tr>
+					<th></th>
+					<th>Belt 1</th>
+					<th>Belt 2</th>
+					<th>Belt 3</th>
+					<th>Belt 4</th>
+					<th>Belt 5</th>
+				</tr>
+				<tr>
+					<th>Unload</th>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+				</tr>
+				<tr>
+					<th>Induct</th>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+				</tr>
+				<tr>
+					<th>Divert</th>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+					<td class="placeable"></td>
+				</tr>
+				<tr>
+					<th>Water Spider</th>
+					<td colspan="5"></td>
+				</tr>
+			</table>
+		</div>
+		<div>
+			<h1>AA's</h1>
+			<div class="flex-container placeable">
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
 
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
 
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
 
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
 
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
-        <Card v-bind:associate="{name:'Hello World', login:'helwor', workdays:'Mon-Fri'}"></Card>
-      </div>
-    </div>
-  </div>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
+				<Card v-bind:associate="{ name: 'Hello World', login: 'helwor', workdays: 'Mon-Fri' }"></Card>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import Card from "../components/Card";
+import Card from '../components/Card';
 export default {
-  name: "task-board",
-  components: {
-    Card
-  },
-  data: function() {
-    return {
-      associate: {
-        name: "Hello World",
-        login: "helworl",
-        workdays: "mon-fri"
-      },
-      laneslots: ["1-2", "3-4", "5-6", "7-8", "9-10"]
-    };
-  }
+	name: 'task-board',
+	components: {
+		Card
+	},
+	data: function() {
+		return {
+			associate: {
+				name: 'Hello World',
+				login: 'helworl',
+				workdays: 'mon-fri'
+			},
+			laneslots: ['1-2', '3-4', '5-6', '7-8', '9-10']
+		};
+	}
 };
 </script>
